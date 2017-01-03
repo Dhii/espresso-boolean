47bd165a722e7b5a8ba56d45943f58715e370957
Miguel Muscat <miguelmuscat93@gmail.com>
2017-01-03 20:05:35 +0100

    Added `AndExpression` class

---
+++ src/Expression/AndExpression.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Dhii\Espresso\Expression;
+
+use \Dhii\Espresso\AbstractLogicalExpression;
+
+/**
+ * A logical AND expression.
+ *
+ * @since [*next-version*]
+ */
+class AndExpression extends AbstractLogicalExpression
+{
+    /**
+     * Constructor.
+     *
+     * @since [*next-version*]
+     *
+     * @param array $terms   [optional] An array of evaluable terms. Default: array()
+     * @param bool  $negated [optional] True to negate the expression, false otherwise.
+     */
+    public function __construct(array $terms = array(), $negated = false)
+    {
+        $this->setTerms($terms)
+            ->setNegated($negated);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @since [*next-version*]
+     */
+    public function _evaluate($left, $right)
+    {
+        return $left && $right;
+    }
+}
