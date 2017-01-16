ef30126552175afacf7ad0b29d7f7171a89649c5
Miguel Muscat <miguelmuscat93@gmail.com>
2017-01-16 16:30:54 +0100

    `AbstractLogicalExpression` extends `AbstractOperatorExpression`

--- src/AbstractLogicalExpression.php
+++ src/AbstractLogicalExpression.php
@@ -2,15 +2,23 @@
 
 namespace Dhii\Espresso;
 
-use \Dhii\Data\ValueAwareInterface;
+use Dhii\Data\ValueAwareInterface;
 
 /**
  * An abstract implementation of a logical expression.
  *
  * @since [*next-version*]
  */
-abstract class AbstractLogicalExpression extends AbstractGenericExpression implements LogicalExpressionInterface
+abstract class AbstractLogicalExpression extends AbstractOperatorExpression implements LogicalExpressionInterface
 {
+
+    /**
+     * Evaluates to false if expression has no terms.
+     *
+     * @since [*next-version*]
+     */
+    const DEFAULT_VALUE = false;
+
     /**
      * The negation flag.
      *
@@ -31,68 +39,12 @@ abstract class AbstractLogicalExpression extends AbstractGenericExpression imple
     }
 
     /**
-     * Sets the expression's negation.
-     *
-     * @since [*next-version*]
-     *
-     * @param bool $negated True to negate the expression, false otherwise.
-     *
-     * @return $this This instance.
-     */
-    public function setNegated($negated)
-    {
-        $this->negated = $negated;
-
-        return $this;
-    }
-
-    /**
      * {@inheritdoc}
      *
      * @since [*next-version*]
      */
-    public function evaluate(ValueAwareInterface $ctx = null)
+    protected function _defaultValue(ValueAwareInterface $ctx = null)
     {
-        $terms = $this->getTerms();
-
-        // If expression has terms, set result to first term
-        $result = count($terms) > 0
-            ? $this->_boolCast($terms[0]->evaluate($ctx))
-            : false;
-
-        for ($i = 1; $i < count($terms); ++$i) {
-            $leftOperand = $result;
-            $rightOperand = $this->_boolCast($terms[$i]->evaluate($ctx));
-            // Evaluate using previous result and next term
-            $result = $this->_compare($leftOperand, $rightOperand);
-        }
-
-        return $result;
-    }
-
-    /**
-     * Compares two operands to evaluate the logical expression.
-     *
-     * @since [*next-version*]
-     *
-     * @param bool $left The left operand.
-     * @param bool $right The right operand.
-     *
-     * @return bool The result.
-     */
-    abstract protected function _compare($left, $right);
-
-    /**
-     * Explicitly casts a value into a boolean.
-     *
-     * @since [*next-version*]
-     *
-     * @param mixed $val The value to cast.
-     *
-     * @return bool The casted boolean value.
-     */
-    protected function _boolCast($val)
-    {
-        return (bool) $val;
+        return static::DEFAULT_VALUE;
     }
 }
